221cae726f8897ce80cc0b35600e007ed3f80b26
Bento Siqueira <bentor.siqueira@gmail.com>
2023-10-08 10:01:53 +0000

    Create test.sh

---
+++ src/test.sh
@@ -0,0 +1,10 @@
+watingPods=0
+while [ $watingPods = 0 ]; do
+  watingPods=$(kubectl get pods | grep web | grep -c Running)
+  echo "Waiting to test."
+  :
+done
+minikube service list
+minikube service web --url
+echo -n "------------------opening the service------------------"
+curl $(minikube service web --url)/metrics
